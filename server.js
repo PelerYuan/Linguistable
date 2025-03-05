8143088e2a4e1d418f75c9695b9a6ecf75612adf
PelerYuan <pelerlittlepig@gmail.com>
2025-03-05 16:29:52 +0800

    dialog done

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const path = require('path');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const { translate } = require('bing-translate-api');
-const fs = require('fs');
 // const { console } = require('inspector');
 
 const app = express();
@@ -16,16 +15,11 @@ app.use(bodyParser.json());
 // 静态文件目录设置（托管 `public` 文件夹）
 app.use(express.static(path.join(__dirname, 'public')));
 
-// 读取语言对照表
-const languages = JSON.parse(fs.readFileSync('languages.json', 'utf-8'));
-
 app.post('/translate', async (req, res) => {
     try {
         const { text, fromLang, toLang } = req.body;
         console.log(`Translate ${text} from '${fromLang}' to '${toLang}'`);
         if (!text) return res.status(400).json({ error: "No text provided" });
-        // const fromLangCode = Object.keys(languages).find(key => languages[key] === fromLang);
-        // const toLangCode = Object.keys(languages).find(key => languages[key] === toLang);
         const result = await translate(text, fromLang, toLang);
         res.json({ translatedText: result.translation });
     } catch (error) {
